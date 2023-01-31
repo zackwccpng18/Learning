a47d1ccfe67bacc763f54df9928047fa26519334
zackwccpng18 <124029109+zackwccpng18@users.noreply.github.com>
2023-01-31 01:19:24 -0600

    Update and rename README.md.py to practice.py

--- practice.py
+++ practice.py
@@ -1,10 +1,5 @@
 # Learning
 
-height = float(input("Enter your height in m: "))
-weight = float(input("Enter your weight in KG: "))
-
-BMI = int((weight/height**2))
-print(BMI)
 
 
 <!DOCTYPE html>
