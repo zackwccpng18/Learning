651f98e8b3afa327488bb5a2743b5c8c127457d9
zackwccpng18 <124029109+zackwccpng18@users.noreply.github.com>
2023-01-31 01:18:39 -0600

    Update README.md.py

--- README.md.py
+++ README.md.py
@@ -5,3 +5,25 @@ weight = float(input("Enter your weight in KG: "))
 
 BMI = int((weight/height**2))
 print(BMI)
+
+
+<!DOCTYPE html>
+<html>
+
+<head>
+<meta charset="utf-8">
+<title>Black Goose Bistro</title>
+</head>
+
+<body>
+Black Goose Bistro
+
+The Restaurant
+The Black Goose Bistro offers casual lunch and dinner fare in a hip atmosphere. The menu changes regularly to highlight the freshest ingredients.
+
+Catering
+You have fun... we'll do the cooking. Black Goose catering can handle events from snacks for bridge club to elegant corporate fundraisers.
+Location and Hours Seekonk, Massachusetts; Monday through Thursday 11am to 9pm, Friday and Saturday, 11am to midnight
+</body>
+
+</html>
