1bbc77cba96eedbc7ddd1079c0ab238818d68bb9
zackwccpng18 <124029109+zackwccpng18@users.noreply.github.com>
2023-02-08 12:27:55 -0600

    Update practice.py

--- practice.py
+++ practice.py
@@ -1,9 +1,11 @@
 # Learning
 
 #OOP
-class Hero:
+class Character:
 	
-	def __init__(self, name, hp, defense, hit_chance, items, speed, resistences)
+	def __init__(self, race, class, hp, defense, hit_chance, items, speed, resistences)
+		self.race = race
+		self.class = class
 		self.name = name
 		self.hp = hp
 		self.defense = defense 
@@ -13,36 +15,20 @@ class Hero:
 		self.resistences = resistences
 	
 	def attk(self, opposing_character):
-		print(chance to hit of character)
-		print(defense of opposing character)
-		print(print hit or miss)
+		print("chance to hit of character")
+		print("defense of opposing character")
+		print("hit or miss")
 		if attack_hit = True:
-			print(attack dmg of character)
-			print(defene of opposing character)
-			print(total dmg done)
+			print("attack dmg of character")
+			print("defene of opposing character")
+			print("total dmg done")
 		else:
-			print(your attack missed)
+			print("your attack missed")
 		
 
-def hero(name):
-	print(name)
-	print(hp)
-	print(defense)
-	print(to hit chance)
-	print(items)
-	print(speed)
-	print(resistences) 
 
-def monster(name):
-	print(name)
-	print(hp)
-	print(defense)
-	print(to hit chance)
-	print(items)
-	print(speed)
-	print(resistences)
 	
 
-hero()
+starter_hero = Character("Human", "fighter", 10, "1", .5, pass, 1, 1.0)
 monster()
 attk(goblin, hero)
